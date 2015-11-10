740acded85a7694b3b73a4cfeab8149cae772395
KevinDEVEAU <deveaukevin77@gmail.com>
2015-11-10 14:34:10 +0100

    test si utilisateur connecté pour mon espace

--- src/main/java/spring/controller/UserController.java
+++ src/main/java/spring/controller/UserController.java
@@ -2,6 +2,9 @@ package spring.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -28,13 +31,13 @@ public class UserController {
 	
 	
 	@RequestMapping(value="toConnect", method=RequestMethod.GET)
-    public String greetingForm(Model model) {
+    public String prepareConnexion(Model model) {
         model.addAttribute("connexion", new Connexion());
         this.utilisateurService.findAll();
         return "connexion";
     }
 	@RequestMapping(value="toConnect", method = RequestMethod.POST)
-    public String toConnect(@ModelAttribute ("connexion") Connexion connexion, Model model) {//page apr�s la connexion
+    public String toConnect(@ModelAttribute ("connexion") Connexion connexion, Model model,HttpServletRequest request) {//page apr�s la connexion
 		//model.addAttribute("person", this.utilisateurService.findAll());
 		List user =this.utilisateurService.findByLogin(connexion.getLogin(),connexion.getPwd());
 		if (user.size()<1){
@@ -44,6 +47,23 @@ public class UserController {
 		 return "connexion";
 		}
 		else{
+			HttpSession session =request.getSession();
+			session.setAttribute("login", connexion.getLogin());
 		return "competitions";}
     }
+	
+	@RequestMapping(value="monEspace", method=RequestMethod.GET)
+    public String monEspace(Model model,HttpServletRequest request) {
+        //model.addAttribute("connexion", new Connexion());
+		HttpSession session =request.getSession();
+		if(session.getAttribute("login")!=null){
+			return "competitions";
+		}
+		else {
+        return this.prepareConnexion(model);
+		}
+    }
+	
+	
+	
 }
--- src/main/webapp/resources/layout/top.jsp
+++ src/main/webapp/resources/layout/top.jsp
@@ -46,7 +46,7 @@
                         <a href="<%=request.getContextPath()%>/robots">Robots</a>
                     </li>
                     <li>
-                        <a href="#">Mon espace</a>
+                        <a href="<%=request.getContextPath()%>/user/monEspace">Mon espace</a>
                     </li>
                     <li>
                         <a href="<%=request.getContextPath()%>/user/connexion">
